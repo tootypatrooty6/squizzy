734d05387b1b0c4ce0b78cfaa128d0ed69266fed
tootypatrooty6 <113472285+tootypatrooty6@users.noreply.github.com>
2023-11-29 17:21:44 -0500

    Update sanityClientConfig.js

--- sanityClientConfig.js
+++ sanityClientConfig.js
@@ -1,6 +1,6 @@
 export const sanityClientConfig = {
   // replace this id with the one you find in /studio/sanity.json
-  projectId: 'puj7p168',
+  projectId: '7g24iz38',
   // make sure that the dataset name is the same as the one in /studio/sanity.json
   dataset: 'production',
   useCdn: false
